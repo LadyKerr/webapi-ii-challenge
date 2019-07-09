995c80d6fe82d66850e0d186a50d5043c743b59c
LadyKerr <kadiekerr@gmail.com>
2019-07-09 15:53:32 -0400

    GET comments

--- data/blog-router.js
+++ data/blog-router.js
@@ -1,10 +1,16 @@
+//import express
 const express = require("express");
 
+//import data
 const DB = require("./db");
 
+//import express router
 const router = express.Router();
 
+//parse json data
 router.use(express.json());
+
+//************ C-R-U-D **********
 
 //display all posts
 router.get("/", (req, res) => {
@@ -37,6 +43,27 @@ router.get("/:id", (req, res) => {
       res
         .status(500)
         .json({ error: "The post information could not be retrieved." });
+    });
+});
+
+//display comments by specified post id
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+
+  DB.findCommentById(id)
+    .then(postComment => {
+      if (postComment && postComment.length) {
+        res.status(200).json(postComment);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved." });
     });
 });
 

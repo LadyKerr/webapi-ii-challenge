4320d95fd0740c8b2d1545eb887b6a074518555e
LadyKerr <kadiekerr@gmail.com>
2019-07-11 14:36:40 -0400

    removed yarn-lock & portal reusable

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 
 const blogRouter = require("./data/blog-router.js");
@@ -6,7 +9,7 @@ const server = express();
 server.use("/api/posts", blogRouter);
 
 //server running
-const port = 8000;
+const port = process.env.PORT || 5000;
 server.listen(port, () =>
-  console.log(`\n*** server is running on port ${port} *** \n`)
+  console.log(`\n*** server is running on http://localhost:${port} *** \n`)
 );
